313c2aaa16fe8c4f7f896df6169e779c558c9c56
Angelo Burog <angelokburog30@gmail.com>
2024-07-23 12:39:03 +0800

    feat: add create and view route

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 const app = express();
 const port = 3000;
+import bodyParser from "body-parser";
 
-app.use(express.urlencoded({ extended: true }));
+var headers = [];
+var bodies = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -10,11 +14,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/view-blogs", (req, res) => {
-  res.render("view.ejs");
+  res.render("view.ejs", { header: headers, body: bodies });
 });
 
 app.get("/create", (req, res) => {
   res.render("create.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  if ((req.body["header"] && req.body["body"]) === "") {
+    res.render("create.ejs", { message: "Please enter a header or body" });
+  } else {
+    const header = req.body["header"];
+    const body = req.body["body"];
+    headers.push(header);
+    bodies.push(body);
+    res.render("create.ejs");
+  }
 });
 
 app.listen(port, () => {
